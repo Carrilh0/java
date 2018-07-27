faa1fe6c7baf00f336e82e645fc4526ba4d46bf7
Leandro Alves <leandro.alves@gmail.com>
2018-07-27 16:43:54 -0300

    Saida de dados

---
+++ TiposPrimitivos.java
@@ -0,0 +1,28 @@
+      /*Classe*/
+public class TiposPrimitivos{
+                     /*Método*/
+    public static void main(String args[]) {
+      String nome = "Leandro";
+      float nota = 8.5f;
+      System.out.format("A nota de %s é %.2f \n", nome,  nota);
+    }
+}
+
+/* SAIDA DE DADOS
+
+        %f = Float
+--Definição de casa decimais--
+    %.2f = .00 casas depois da vírgula
+
+--Definição de Strings--
+    %s = String
+
+--Concatenação--
+  String teste = 1;
+
+  Float teste1 = 2f;
+
+                      variável que vem primeiro
+    System.out.print("%s %.2f", teste, teste1);
+
+*/
