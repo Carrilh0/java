e115f1b3a7cf3a798e66c71769ac9d5ff6ddf8f2
leoalves100 <41408250+leoalves100@users.noreply.github.com>
2018-09-20 16:01:37 -0300

    Atividade - CalculoCirculo

---
+++ EstruturaCondicional.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class CalculoCirculo{
+	public static void main(String args[]){
+	
+	public static final float pi = 3.14f; //Constante
+	
+	float raio, area;
+	
+	Scanner area = new Scanner();
+	
+	System.out.println("Digite a raio: ");
+	System.out.println("=>");
+	reio = area.nextFloat();
+	
+	area = (pi*(raio^2));
+	
+	System.out.println("O valor da área é: " + area);
+	}
+}
